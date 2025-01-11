1f778452edfe3060e08b760cfd1174126a18d638
Abhimanyu Rathor <abhimanyurathor99@gmail.com>
2025-01-11 17:57:18 +0530

    11-01-25

---
+++ src/JavaPrograms/Lab001_Sout.java
@@ -0,0 +1,7 @@
+package JavaPrograms;
+
+public class Lab001_Sout {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/JavaPrograms/Lab002_comments.java
@@ -0,0 +1,13 @@
+package JavaPrograms;
+
+import java.sql.SQLOutput;
+
+public class Lab002_comments {
+    public static void main(String[] args) {
+        //System.out.println("Hello World");
+        System.out.println(2+2);
+        /*System.out.println(3+3);
+        System.out.println(4+5);
+        System.out.println(5+6);*/
+    }
+}
